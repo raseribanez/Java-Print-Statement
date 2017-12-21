1eaa1d2d78d51270ad2e94cad39028f5e6481b77
Ben Woodfield <raser.13@hotmail.com>
2017-12-21 05:06:39 +0000

    Add files via upload

---
+++ PrintMessage.java
@@ -0,0 +1,6 @@
+public class BenJava{
+	// The class name above (BenJava) needs to be the same name as the filename
+	public static void main (String[] args){
+		System.out.println("Hi my name is Ben, and I am writing this program in Java!!!");
+	}
+}
