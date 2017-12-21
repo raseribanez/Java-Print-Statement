9d5a0c20ed15ffd27bd58d957189793f85cefb42
Ben Woodfield <raser.13@hotmail.com>
2017-12-21 05:24:08 +0000

    Update PrintMessage.java

--- PrintMessage.java
+++ PrintMessage.java
@@ -1,5 +1,5 @@
 public class PrintMessage{
-	// The class name above (BenJava) needs to be the same name as the filename
+	// The class name above (PrintMessage) needs to be the same name as the filename
 	public static void main (String[] args){
 		System.out.println("Hi my name is Ben, and I am writing this program in Java!!!");
 	}
